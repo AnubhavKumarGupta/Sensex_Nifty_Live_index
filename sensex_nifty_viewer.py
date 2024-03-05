6824ea64767a7ff5d14f372bd78b5aa7aa30e270
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2024-03-06 01:58:52 +0530

    Add files via upload

--- sensex_nifty_viewer.py
+++ sensex_nifty_viewer.py
@@ -1,12 +1,28 @@
 import tkinter as tk
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
 from tkinter import ttk, StringVar
 
 
 mainwindow = tk.Tk()
 mainwindow.title("Sensex/Nifty Live Stock Market Update")
 
-columns = ("Nifty", "Nifty_Time", "Sensex", "Sensex_Time")
 
+def get_nifty_date_from_weburl():
+    nifty_url = "https://www.moneycontrol.com/indian-indices/nifty-50-9.html"
+    page = urlopen(nifty_url)
+    soup = BeautifulSoup(page , 'html.parset')
+
+
+    sensex_url = "https://www.moneycontrol.com/indian-indices/sensex-4.html"
+
+
+
+
+
+
+
+columns = ("Nifty", "Nifty_Time", "Sensex", "Sensex_Time")
 tree = ttk.Treeview(mainwindow, columns=columns, show="headings")
 tree.heading("Nifty", text="Nifty")
 tree.heading("Nifty_Time", text="Nifty_Time")
